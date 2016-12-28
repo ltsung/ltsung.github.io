2c8ec53a73ce5150eac7a7e6a3defffde4426e31
Lauren Tsung <ltsung@sal-et-lux.local>
2016-12-27 21:39:44 -0800

    New 2016 version

---
+++ Portfolio_Dev/app/scripts/main.js
@@ -0,0 +1,26 @@
+var lastScrollTop = 0;
+
+
+$(window).scroll(function(event) {
+
+   var st = $(this).scrollTop();
+
+   if (st > lastScrollTop){
+       // Down
+       $('.navbar-wrapper').css('position', 'absolute');
+       $('.navbar-wrapper').fadeOut();
+
+   } else {
+      // Up
+       $('.navbar-inverse').css('cssText', 'background-color: rgba(0,0,0,0.5) !important');
+       $('.navbar-wrapper').css('position', 'fixed');
+       $('.navbar-wrapper').fadeIn();
+       
+       // At very top
+       if (st == 0) {
+         $('.navbar-inverse').css('cssText', 'background-color: rgba(0,0,0,0) !important');
+       }
+   }
+
+   lastScrollTop = st;
+});
