060f93e1e163c956fc16efab687817fd7d12d033
Lauren Tsung <ltsung@sal-et-lux.local>
2017-01-03 16:23:45 -0800

    New portfolio

--- Portfolio_Dev/app/scripts/main.js
+++ Portfolio_Dev/app/scripts/main.js
@@ -1,29 +1,50 @@
 var lastScrollTop = 0;
+var st = 0;
+
+$( window ).resize(function() {
+
+   if (st == 0 && $(window).width() > 768) {
+     $('.navbar-inverse').css('cssText', 'background-color: rgba(0,0,0,0) !important');
+   }
+   
+   if ($(window).width() <= 768) {
+       $('.navbar-inverse').css('cssText', 'background-color: #1C1E37 !important');   
+   }
+});
 
 
 $(window).scroll(function(event) {
 
-   var st = $(this).scrollTop();
+   st = $(this).scrollTop();
 
    if (st > lastScrollTop){
        // Down
        
        if (st > 50) {	       
-	       $('.navbar-wrapper').css('position', 'absolute');
 	       $('.navbar-wrapper').fadeOut();
+	       
+	       setTimeout(function() { 
+               $('.navbar-wrapper').css('position', 'absolute');
+	       }, 400);
        }
-
+       
    } else {
       // Up
-       $('.navbar-inverse').css('cssText', 'background-color: rgba(0,0,0,0.5) !important');
+       $('.navbar-inverse').css('cssText', 'background-color: #1C1E37 !important');
+     
        $('.navbar-wrapper').css('position', 'fixed');
+       
        $('.navbar-wrapper').fadeIn();
        
        // At very top
-       if (st == 0) {
+       if (st == 0 && $(window).width() > 768) {
          $('.navbar-inverse').css('cssText', 'background-color: rgba(0,0,0,0) !important');
        }
    }
 
    lastScrollTop = st;
 });
+
+function fadeIn(obj) {
+    $(obj).fadeIn(1000);
+}
